3dc06878ad63baf18577344f9a2cdba06954f09b
GoatChan <88063767+CarlixDevnt@users.noreply.github.com>
2022-12-07 14:08:05 +0100

    Update hanoi.js

--- hanoi.js
+++ hanoi.js
@@ -1,15 +1,15 @@
-function hanoi(discos, origen, destino, auxiliar)
+function hanoi(disco, origen, destino, auxiliar)
 {
-    if(discos == 1) {
+    if(disco == 1) {
         // Mover 1 disco desde la vara 1 hasta la 3
-        console.log("Mueve el disco ", discos, " desde la vara ", origen, " hasta la vara ", destino);
+        console.log(`Mueve el disco ${disco} desde la vara ${origen} hasta la vara ${destino}`);
     } else {
         // Mover n-1 discos desde la vara origen a la auxiliar
-        hanoi(discos-1, origen, auxiliar, destino);
-        console.log("Mueve el disco ", discos, " desde la vara ", origen, " hasta la vara ", destino);
+        hanoi(disco-1, origen, auxiliar, destino);
+        console.log(`Mueve el disco ${disco} desde la vara ${origen} hasta la vara ${destino}`);
         // Mover esos discos de la vara auxiliar a la vara destino
-        hanoi(discos-1, auxiliar, destino, origen);
+        hanoi(disco-1, auxiliar, destino, origen);
     }
 }
 
-hanoi(3, 1, 3, 2);
+hanoi(3, "origen", "destino", "auxiliar");
